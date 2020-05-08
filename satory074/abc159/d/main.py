f16bc42c6000e35c0dd2776cf4b4268d8125ab63
satory074 <satory074@gmail.com>
2020-05-08 20:23:44 +0900

    Add abc159 d

--- satory074/abc159/d/main.py
+++ satory074/abc159/d/main.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
-import collections
-import itertools as it
-import math
-#import numpy as np
- 
-#  = input()
-#  = int(input())
-#  = map(int, input().split())
-#  = list(map(int, input().split()))
-#  = [int(input()) for i in range(N)]
-#
-# c = collections.Counter()
+import collections as cl
+from math import factorial
+
+def comb(n, r):
+    if n >= r:
+        return factorial(n) // (factorial(n - r) * factorial(r))
+    else:
+        return 0
+  
+n = int(input())
+a = list(map(int, input().split()))
+
+clc = cl.Counter(a)
+
+# 1. 数字が等しいペアを選ぶ総数
+npair = 0
+for (k, v) in clc.items():
+    npair += comb(v, 2)
+
+# 2. (取り除いた数字と同じ数字を選ぶ総数) - (1)
+for a_ in a:
+    print(npair - (clc[a_] - 1))
