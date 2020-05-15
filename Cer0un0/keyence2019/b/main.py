1e302003726b1a90dcb0afd8b5105fd7323832ac
Cer0un0 <wataru.mrt@gmail.com>
2020-05-15 22:57:37 +0900

    社畜精進 #4

--- Cer0un0/keyence2019/b/main.py
+++ Cer0un0/keyence2019/b/main.py
@@ -13,10 +13,9 @@ for i in range(len(key)+1):
     K.append([key[:i],key[i:]])
 
 for i in range(len(K)):
-    if K[i][0] in S and K[i][1] in S:
-        if S[0] == 'k' and S[-1] == 'e':
-            print('YES')
-            exit()
+    if S[:len(K[i][0])] == K[i][0] and S[len(S)-len(K[i][1]):] == K[i][1]:
+        print('YES')
+        exit()
 print('NO')
 
 
