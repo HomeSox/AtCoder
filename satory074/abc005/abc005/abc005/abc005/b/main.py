f5993728f3e5c6aa199373691e5dbaf4400e94f4
satory074 <satory074@gmail.com>
2020-05-17 20:54:37 +0900

    Add: abc005 ab

---
+++ satory074/abc005/abc005/abc005/abc005/b/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import collections
+import itertools as it
+import math
+#import numpy as np
+ 
+#  = input()
+n  = int(input())
+#  = map(int, input().split())
+#  = list(map(int, input().split()))
+t  = [int(input()) for i in range(n)]
+#
+# c = collections.Counter()
+
+print(min(t))
