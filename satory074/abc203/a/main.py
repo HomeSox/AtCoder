9f616c85644b8a407ae1bdbce7d1cd89eff0b4f3
satory074 <satory074@gmail.com>
2021-06-13 23:24:53 +0900

    Add: 203 a

---
+++ satory074/abc203/a/main.py
@@ -0,0 +1,13 @@
+import collections as cl
+
+a, b, c = map(int, input().split())
+
+clcm = cl.Counter([a, b, c]).most_common()
+n = clcm[0][1]
+
+if n == 1:
+    print(0)
+if n == 2:
+    print(clcm[1][0])
+if n == 3:
+    print(clcm[0][0])
