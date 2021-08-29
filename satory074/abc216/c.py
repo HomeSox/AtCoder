5b386ca0b0e02c8bcfc8c895ea81c303dd918bb2
satory074 <satory074@gmail.com>
2021-08-29 23:56:52 +0900

    Add: abc216 a-c

---
+++ satory074/abc216/c.py
@@ -0,0 +1,18 @@
+N = int(input())
+
+ans = []
+while N != 0 and N != 1:
+  if N % 2 == 0:
+    N = N // 2
+    ans.append('B')
+  else:
+    N -= 1
+    ans.append('A')
+
+if N == 0:
+  ans.append('B')
+  ans.append('A')
+else:
+  ans.append('A')
+
+print(''.join(ans[::-1]))
