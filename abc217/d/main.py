f35763080740029483e9012640603a449690aae5
satory074 <satory074@gmail.com>
2021-09-05 08:35:43 +0900

    Add: abc217/d

--- abc217/d/main.py
+++ abc217/d/main.py
@@ -1,17 +1,14 @@
+import array
 import bisect
 
 L, Q = map(int, input().split())
 
-cutx = [0, L]
+cutx = array.array('i', [0, L])
 for _ in range(Q):
     c, x = map(int, input().split())
 
-    # print(idx, cutx)
     if c == 1:
-        bisect.insort_left(cutx, x)
-    elif c == 2:
-        if not cutx:
-            print(L)
-            continue
+        bisect.insort(cutx, x)
+    else:
         idx = bisect.bisect(cutx, x)
         print(cutx[idx] - cutx[idx-1])
