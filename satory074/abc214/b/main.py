45985496d381b4f6de920685ed022c5ea4d7a58e
satory074 <satory074@gmail.com>
2021-09-06 20:47:39 +0900

    Add: abc214/b

--- satory074/abc214/b/main.py
+++ satory074/abc214/b/main.py
@@ -1,1 +1,12 @@
-# = map(int, input())
+S, T = map(int, input().split())
+
+answer = 0
+for a in range(S+1):
+    for b in range(S+1):
+        for c in range(S+1):
+            if a + b + c <= S and a * b * c <= T:
+                answer += 1
+            else:
+                break
+
+print(answer)
