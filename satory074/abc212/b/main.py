8fe04acf5346dadf0cf703cfb3d7b166ed7ea288
satory074 <satory074@gmail.com>
2021-09-10 14:28:38 +0900

    Add: abc212 b

--- satory074/abc212/b/main.py
+++ satory074/abc212/b/main.py
@@ -1,1 +1,16 @@
-# = map(int, input())
+X = list(map(int, list(input())))
+
+if len(set(list(X))) == 1:
+    print('Weak')
+    exit()
+
+for x1, x2 in zip(X[:-1], X[1:]):
+    if (10 if x2 == 0 else x2) - x1 == 1:
+        None
+    else:
+        break
+else:
+    print('Weak')
+    exit()
+
+print('Strong')
