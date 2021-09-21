c0966564eb691bf4f33cb1e732e62c80239d02f0
satory074 <satory074@gmail.com>
2021-09-21 17:24:31 +0900

    Add: abc219 c

--- satory074/abc219/c/main.py
+++ satory074/abc219/c/main.py
@@ -1,25 +1,5 @@
-X = list(input())
+X = input()
 N = int(input())
-# S = [input() for _ in range(N)]
-
-# print(ord('z'), ord('A'))
-S = []
-for _ in range(N):
-    w = ''
-    for s in list(input()):
-        w += X[ord(s) - 97]
-    # print(w)
-
-    S.append(w)
-
-S.sort()
-# print()
-
-for s in S:
-    w = ''
-    for s_ in list(s):
-        w += chr(X.index(s_) + 97)
-
-    print(w)
-
-print(chr(0+97))
+S = [input() for _ in range(N)]
+S = sorted(S, key=lambda x: [X.index(c) for c in x])
+[print(i) for i in S]
