67b9a23ca11562289d54bb0102cd1b70122e6e3d
satory074 <satory074@gmail.com>
2022-03-23 19:54:48 -0700

    Add: abc014/2

--- satory074/abc014/b/main.py
+++ satory074/abc014/b/main.py
@@ -1,24 +1,9 @@
-#!/usr/bin/env python3
-import collections
-import itertools as it
-import math
-#import numpy as np
- 
-#  = input()
-#  = int(input())
-n, x  = map(int, input().split())
-a  = list(map(int, input().split()))
-#  = [int(input()) for i in range(N)]
-#
-# c = collections.Counter()
-
-b = f'{x:len(a)b}'
+n, X = map(int, input().split())
+a = list(map(int, input().split()))
 
 ans = 0
-print(b)
-for i in range(min(len(b), len(a))):
-    if b[i] == '1':
-       ans += a[i] 
+for i in range(n):
+    if (X >> i) & 1:
+        ans += a[i]
 
 print(ans)
-
