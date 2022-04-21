254aea82d4e87600ca897358b57812054f658149
satory074 <satory074@gmail.com>
2022-04-21 11:36:07 +0900

    Add: abc220/d

--- satory074/abc220/d/main.py
+++ satory074/abc220/d/main.py
@@ -1,35 +1,19 @@
+from collections import deque
+
 N = int(input())
 A = list(map(int, input().split()))
 MOD = 998244353
 
-answer = [0 for _ in range(10)]
+que = deque(A)
 
-keisuu_table = [0 for _ in range(10)]
-keisuu_table[A[0]] = 1
-for a in A[1:]:
-    num_kouho = []
-    for n, k in enumerate(keisuu_table):
-        if k == 0:
-            continue
-        # print(n, k)
+dp = [[0 for _ in range(10)] for _ in range(N)]
+dp[0][que.popleft()] = 1
 
-        result_plus = (n + a) % 10
-        result_mult = (n * a) % 10
+for i in range(1, N):
+    a = que.popleft()
+    for j in range(10):
+        if dp[i - 1][j] != 0:
+            dp[i][(j + a) % 10] = (dp[i][(j + a) % 10] + dp[i - 1][j]) % MOD
+            dp[i][(j * a) % 10] = (dp[i][(j * a) % 10] + dp[i - 1][j]) % MOD
 
-        for _ in range(k):
-            num_kouho.append(result_plus)
-            num_kouho.append(result_mult)
-
-    keisuu_table = [0 for _ in range(10)]
-    for n in list(set(num_kouho)):
-        keisuu_table[n] += num_kouho.count(n)
-
-    # print(num_kouho)
-[print(k) for k in keisuu_table]
-
-
-
-
-
-
-
+[print(dp[N - 1][k]) for k in range(10)]
