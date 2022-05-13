0890825297cfc0d98e55192a8473516dd367b63e
satory074 <satory074@gmail.com>
2022-05-13 19:52:21 +0900

    Add: abc081/b

--- satory074/abc081/b/main.py
+++ satory074/abc081/b/main.py
@@ -1,3 +1,22 @@
 import math
 import collections as cl
 from collections import deque
+
+N = int(input())
+A = list(map(int, input().split()))
+
+ans = 0
+for _ in range(10 ** 9):
+    for a in A:
+        if a % 2 == 1:
+            break
+    else:
+        ans += 1
+
+        A = [a // 2 for a in A]
+
+        continue
+
+    break
+
+print(ans)
