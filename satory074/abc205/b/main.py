ed596996d22f5aef8b3079fe41a2f60bed5ca727
satory074 <satory074@gmail.com>
2022-06-30 10:50:33 +0900

    feat: abc205/b-c

--- satory074/abc205/b/main.py
+++ satory074/abc205/b/main.py
@@ -1,3 +1,13 @@
 import math
 import collections as cl
 from collections import deque
+
+N = int(input())
+A = list(map(int, input().split()))
+
+A.sort()
+
+if A == list(range(1, N+1)):
+    print('Yes')
+else:
+    print('No')
--- satory074/abc205/c/main.py
+++ satory074/abc205/c/main.py
@@ -1,3 +1,21 @@
 import math
 import collections as cl
 from collections import deque
+
+A, B, C = map(int, input().split())
+
+sign_ac = -1 if A < 0 and C % 2 == 1 else 1
+sign_bc = -1 if B < 0 and C % 2 == 1 else 1
+
+if sign_ac == sign_bc:
+    if abs(A) > abs(B):
+        print('>')
+    elif abs(A) < abs(B):
+        print('<')
+    else:
+        print('=')
+else:
+    if sign_ac > sign_bc:
+        print('>')
+    else:
+        print('<')
