fe1257a741c343f7521785ce648f570c454bac74
satory074 <satory074@gmail.com>
2022-07-11 10:42:55 +0900

    feat: abc213/c-d

--- satory074/abc213/c/main.py
+++ satory074/abc213/c/main.py
@@ -1,1 +1,20 @@
-# = map(int, input())
+H, W, N = map(int, input().split())
+
+A = []
+B = []
+for _ in range(N):
+    a, b = map(int, input().split())
+
+    A.append(a)
+    B.append(b)
+
+dict_A = {}
+for i, a in enumerate(sorted(list(set(A)))):
+    dict_A[a] = i + 1
+
+dict_B = {}
+for i, b in enumerate(sorted(list(set(B)))):
+    dict_B[b] = i + 1
+
+for a, b in zip(A, B):
+    print(dict_A[a], dict_B[b])
