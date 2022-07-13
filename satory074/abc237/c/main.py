2090f7032bf752db371835373ef27e9b62ff4f81
satory074 <satory074@gmail.com>
2022-07-13 14:30:49 +0900

    feat: abc237/c

--- satory074/abc237/c/main.py
+++ satory074/abc237/c/main.py
@@ -1,23 +1,12 @@
-import collections as cl
-
 S = input()
 
-rS = S[::-1]
+_rstrip = S.rstrip("a")
+lrstrip = _rstrip.lstrip("a")
 
-n_a = 0
-for s in rS:
-    if s == "a":
-        n_a += 1
-    else:
-        break
+n_ra = len(S) - len(_rstrip)
+n_la = len(_rstrip) - len(lrstrip)
 
-
-for i in range(n_a + 1):
-    if S == rS:
-        print("Yes")
-        break
-    else:
-        S = "a" + S
-        rS = rS + "a"
+if lrstrip == lrstrip[::-1] and n_la <= n_ra:
+    print("Yes")
 else:
     print("No")
