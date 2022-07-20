697675df2598df0acbf94dc91029c989d4960cd5
satory074 <satory074@gmail.com>
2022-07-20 23:42:16 +0900

    feat: abc255/b

--- satory074/abc255/b/main.py
+++ satory074/abc255/b/main.py
@@ -1,2 +1,24 @@
 import collections as cl
 import math
+
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+X = []
+Y = []
+for _ in range(N):
+    x, y = list(map(int, input().split()))
+    X.append(x)
+    Y.append(y)
+
+ans = 0
+for i in range(N):
+    d = 10**18
+    for a in A:
+        d_ = ((X[i] - X[a - 1]) ** 2 + (Y[i] - Y[a - 1]) ** 2) ** 0.5
+
+        d = min(d, d_)
+
+    ans = max(ans, d)
+
+print(ans)
