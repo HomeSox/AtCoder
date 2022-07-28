f7ef736befc2721e34913aeeca14877f01527cff
satory074 <satory074@gmail.com>
2022-07-28 11:23:11 +0900

    feat: abc248/c-d

--- satory074/abc248/c/main.py
+++ satory074/abc248/c/main.py
@@ -1,22 +1,15 @@
-import collections as cl
-import math
-
-
-def combinations_count(n, r):
-    return math.factorial(n) // (math.factorial(n - r) * math.factorial(r))
-
-
+MOD = 998244353
 N, M, K = map(int, input().split())
 
+dp = [[0] * (K + 1) for _ in range(N + 1)]
+dp[0][0] = 1
 
-if N > K:
-    print(0)
-    exit()
+for i in range(N):
+    for j in range(K + 1):
+        for k in range(1, M + 1):
+            if j - k >= 0:
+                dp[i + 1][j] += dp[i][j - k]
 
+            dp[i + 1][j] %= MOD
 
-ans = 1
-MOD = 998244353
-for i in range(N + 1, K + 1):
-    ans += combinations_count(N, i) % MOD
-
-print(ans)
+print(sum(dp[N]) % MOD)
--- satory074/abc248/d/main.py
+++ satory074/abc248/d/main.py
@@ -1,2 +1,18 @@
-import collections as cl
-import math
+import bisect
+
+N = int(input())
+A = list(map(int, input().split()))
+Q = int(input())
+
+indexes = [[] for _ in range(N + 1)]
+
+for i, a in enumerate(A):
+    indexes[a].append(i + 1)
+
+for _ in range(Q):
+    L, R, X = map(int, input().split())
+
+    st = bisect.bisect_left(indexes[X], L)
+    gl = bisect.bisect_right(indexes[X], R)
+
+    print(gl - st)
