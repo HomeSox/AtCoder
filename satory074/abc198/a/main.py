d5ff8eac057d611470322af908edeed2b6bc392d
satory074 <satory074@gmail.com>
2022-08-08 09:57:28 +0900

    feat: abc198/a-b

--- satory074/abc198/a/main.py
+++ satory074/abc198/a/main.py
@@ -1,2 +1,5 @@
 import collections as cl
 from collections import deque
+
+N = int(input())
+print(N-1)
--- satory074/abc198/b/main.py
+++ satory074/abc198/b/main.py
@@ -1,2 +1,12 @@
 import collections as cl
 from collections import deque
+
+N = input()
+
+for i in range(100):
+    s = f"{N[::-1]}{'0'*i}"
+    if s == s[::-1]:
+        print("Yes")
+        break
+else:
+    print("No")
