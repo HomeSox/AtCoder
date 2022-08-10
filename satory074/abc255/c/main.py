9e59a1735d2aea6769938dafbabf0f04b652139c
satory074 <satory074@gmail.com>
2022-08-10 12:02:47 +0900

    feat: abc255/c-d

--- satory074/abc255/c/main.py
+++ satory074/abc255/c/main.py
@@ -1,2 +1,18 @@
-import collections as cl
-import math
+X, A, D, N = map(int, input().split())
+
+if D < 0:
+    fi = A + D * (N - 1)
+    A = fi
+    D *= -1
+
+fi = A + D * (N - 1)
+st = A
+
+if st <= X <= fi:
+    m = 0 if X == 0 else (X - st) % D
+
+    exit(print(min(m, D - m)))
+elif X < st:
+    exit(print(st - X))
+else:
+    exit(print(X - fi))
--- satory074/abc255/d/main.py
+++ satory074/abc255/d/main.py
@@ -1,2 +1,24 @@
-import collections as cl
-import math
+import bisect
+
+N, Q = map(int, input().split())
+A = list(map(int, input().split()))
+
+sortedA = sorted(A)
+
+# 累積和
+cumA = [0] * (N + 1)
+for i in range(len(sortedA)):
+    cumA[i + 1] = cumA[i] + sortedA[i]
+
+# print(sortedA)
+# print(cumA)
+
+for _ in range(Q):
+    X = int(input())
+    idx = bisect.bisect_left(sortedA, X)
+
+    absleft = abs(X * idx - cumA[idx])
+    absright = abs(X * (N - idx) - (cumA[-1] - cumA[idx]))
+
+    # print(X, idx, absleft, absright)
+    print(absleft + absright)
