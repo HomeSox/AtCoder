c00e5c8f2b53361859dd58bf46b55138a8cda47d
satory074 <satory074@gmail.com>
2022-08-12 23:05:16 +0900

    abc243/b

--- satory074/abc243/b/main.py
+++ satory074/abc243/b/main.py
@@ -1,2 +1,19 @@
 import collections as cl
 import math
+
+N = int(input())
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+ans1 = 0
+ans2 = 0
+for a, b in zip(A, B):
+    if a == b:
+        ans1 += 1
+    else:
+        if a in B:
+            ans2 += 1
+
+print(ans1)
+print(ans2)
