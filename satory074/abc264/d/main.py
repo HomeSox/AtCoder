4b42322107201806cb44260559dfdf8549315f2f
satory074 <satory074@gmail.com>
2022-08-14 22:46:38 +0900

    feat: abc264/d

--- satory074/abc264/d/main.py
+++ satory074/abc264/d/main.py
@@ -1,2 +1,19 @@
 import collections as cl
 import math
+
+S = input()
+
+awesome = "atcoder"
+l = [awesome.index(s) for s in S]
+
+
+def get_inversion_count(l):
+    count = 0
+    for i in range(len(l)):
+        for j in range(i + 1, len(l)):
+            if l[i] > l[j]:
+                count += 1
+    return count
+
+
+print(get_inversion_count(l))
