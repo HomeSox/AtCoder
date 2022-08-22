e0faf2e82f6212983d07e4b2cffcc843c5510541
satory074 <satory074@gmail.com>
2022-08-22 09:57:45 +0900

    feat: abc226/c

--- satory074/abc226/c/main.py
+++ satory074/abc226/c/main.py
@@ -1,2 +1,20 @@
-import collections as cl
-from collections import deque
+N = int(input())
+
+times = [0] * N
+W = []
+for i in range(N):
+    T, K, *A = list(map(int, input().split()))
+    times[i] = T
+    W.append([a - 1 for a in A])
+
+skill_tree = [False] * N
+skill_tree[N-1] = True
+
+ans = 0
+for i in range(N-1, -1, -1):
+    if skill_tree[i]:
+        ans += times[i]
+        for j in W[i]:
+            skill_tree[j] = True
+
+print(ans)
