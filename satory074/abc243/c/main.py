50e66faabe06d595b3ec363da09af5f833692405
satory074 <satory074@gmail.com>
2022-10-13 11:30:36 +0900

    feat: abc243/c-d

--- satory074/abc243/c/main.py
+++ satory074/abc243/c/main.py
@@ -1,2 +1,30 @@
-import collections as cl
-import math
+N = int(input())
+XY = [list(map(int, input().split())) for _ in range(N)]
+S = input()
+
+Y = [x[1] for x in XY]
+
+search = {}
+for y in set(Y):
+    search[y] = {
+        'R': 10**10,
+        'L': -1,
+    }
+
+for i, s in enumerate(S):
+    x, y = XY[i]
+
+    if s == 'R':
+        search[y]['R'] = min(search[y]['R'], x)
+        continue
+
+    if s == 'L':
+        search[y]['L'] = max(search[y]['L'], x)
+        continue
+
+for y in set(Y):
+    if search[y]['L'] >= search[y]['R']:
+        print('Yes')
+        exit()
+
+print('No')
--- satory074/abc243/d/main.py
+++ satory074/abc243/d/main.py
@@ -1,2 +1,21 @@
-import collections as cl
-import math
+from collections import deque
+
+N, X = map(int, input().split())
+S = input()
+
+ans = deque(bin(X))
+for s in S:
+    if s == 'U':
+        deque.pop(ans)
+        continue
+
+    if s == 'L':
+        deque.append(ans, '0')
+        continue
+
+    if s == 'R':
+        deque.append(ans, '1')
+        continue
+
+
+print(int(''.join(ans), 2))
