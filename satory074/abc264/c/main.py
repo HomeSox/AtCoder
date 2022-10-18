714222f1d171d0d311d871a530da07af6b7f341b
satory074 <satory074@gmail.com>
2022-10-18 13:56:08 +0900

    feat: abc264/c

--- satory074/abc264/c/main.py
+++ satory074/abc264/c/main.py
@@ -7,37 +7,32 @@ A = [list(input().split()) for _ in range(H)]
 H2, W2 = map(int, input().split())
 B = [list(input().split()) for _ in range(H2)]
 
+# bit全探索
+for ih in range(2**H):
+    bh = bin(ih)[2:].zfill(H)
+    for iw in range(2**W):
+        bw = bin(iw)[2:].zfill(W)
 
-indexes = []
-for h in range(H):
-    l = []
-    for w in range(W):
-        for h2 in range(H2):
-            for w2 in range(W2):
-                if A[h][w] == B[h2][w2]:
-                    l.append((h, w))
-    indexes.append(list(set(l)))
+        A2 = []
+        # print(bh, bw)
+        for h, bh_ in enumerate(bh):
+            l = []
+            if bh_ == '0':
+                continue
 
-print(indexes)
-exit()
+            for w, bw_ in enumerate(bw):
+                if bw_ == '0':
+                    continue
 
+                l.append(A[h][w])
 
-ans = []
-for idx in indexes2:
-    for i, b in enumerate(B):
-        # print(idx, b)
-        if idx == b:
-            ans.append(i)
-
-print(ans)
+            if l:
+                A2.append(l)
 
 
-if len(set(ans)) != 2:
-    print("No")
-    exit()
+        if A2 == B:
+            print('Yes')
+            exit()
 
-print(list(set(ans)), list(range(H2)))
-if list(set((ans))) == list(range(H2)):
-    print("Yes")
-else:
-    print("No")
+print('No')
+
