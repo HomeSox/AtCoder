a80715cf94e7c58abb71ee9be2c9082b9c1b9f31
satory074 <satory074@gmail.com>
2023-03-27 23:22:15 +0900

    abc295/d

--- satory074/abc295/d/main.py
+++ satory074/abc295/d/main.py
@@ -1,3 +1,16 @@
-S = list(input().split())
-is_even = [True for _ in range(2 ** 10)]
-is_even[0] = False
+S = input()
+
+table = [0 for _ in range(2 ** 10)]
+table[0] = 1
+
+bit = 0
+for s in S:
+    bit ^= 2 ** int(s)
+
+    table[bit] += 1
+
+ans = 0
+for t in table:
+    ans += t * (t - 1) // 2
+
+print(ans)
