85757b400a63d1da8eebce158ee4dbc3648e7cae
satory074 <satory074@gmail.com>
2023-04-02 12:50:33 +0900

    abc212

--- satory074/abc212/d/main.py
+++ satory074/abc212/d/main.py
@@ -1,1 +1,20 @@
-# = map(int, input())
+import heapq
+
+Q = int(input())
+
+heap = []
+plus = 0
+for _ in range(Q):
+    query = list(map(int, input().split()))
+
+    if query[0] == 1:
+        heapq.heappush(heap, query[1] - plus)
+        continue
+
+    if query[0] == 2:
+        plus += query[1]
+        continue
+
+    if query[0] == 3:
+        print(heapq.heappop(heap) + plus)
+        continue
