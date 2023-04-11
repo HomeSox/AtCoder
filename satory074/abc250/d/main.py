0ceb56db97431a6350d77d004e709e863956e701
satory074 <satory074@gmail.com>
2023-04-12 01:11:01 +0900

    abc250/d

--- satory074/abc250/d/main.py
+++ satory074/abc250/d/main.py
@@ -1,2 +1,22 @@
-import collections as cl
-import math
+import bisect
+
+N = int(input())
+n = 10 ** 6 + 10
+
+prime = {i for i in range(2, n)}
+
+for i in range(2, n):
+    if i in prime:
+        for j in range(i * 2, n, i):
+            if j in prime:
+                prime.remove(j)
+
+prime = list(prime)
+prime.sort()
+
+ans = 0
+for p in prime[::-1]:
+    div = N // pow(p, 3)
+    ans += bisect.bisect(prime, min(div, p - 1))
+
+print(ans)
