754106d2f25e0c3025ba4669299ceff5022c090f
satory074 <satory074@gmail.com>
2023-05-06 23:34:38 +0900

    arc116/b

--- satory074/arc116/b/main.py
+++ satory074/arc116/b/main.py
@@ -1,1 +1,14 @@
-# = map(int, input())
+N = int(input())
+A = list(map(int, input().split()))
+MOD = 998244353
+
+A.sort(reverse=True)
+
+ans = 0
+s = 0
+for a in A:
+    ans += a * (a + s)
+    ans %= MOD
+    s = (s * 2 + a) % MOD
+
+print(ans)
