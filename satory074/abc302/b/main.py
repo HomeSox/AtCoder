41e63578d1c27f81cbd0a19ca67178efba0fe2fb
satory074 <satory074@gmail.com>
2023-07-07 23:44:51 +0900

    Refactor find_snuke function to use input instead of hard-coded grid

--- satory074/abc302/b/main.py
+++ satory074/abc302/b/main.py
@@ -1,19 +1,29 @@
-def find_snuke(grid):
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == 's':
-                print(i,j )
-                for dx, dy in directions:
-                    try:
-                        if ''.join([grid[i + k * dx][j + k * dy] for k in range(5)]) == 'snuke':
-                            return [(i + k * dx, j + k * dy) for k in range(5)]
-                    except IndexError:
-                        pass
-    return []
+from itertools import product
 
-# Test case
-grid = [
-    list('s')
-]
-print(find_snuke(grid))
+H, W = map(int, input().split())
+S = [input() for _ in range(H)]
+
+direction = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+for i, j in product(range(H), range(W)):
+    for dx, dy in direction:
+
+        t = ""
+        for k in range(5):
+            x = i + dx * k
+            y = j + dy * k
+
+            if 0 <= x < H and 0 <= y < W:
+                t += S[x][y]
+
+        # print(t)
+
+        if t == "snuke":
+            for k in range(5):
+                x = i + dx * k
+                y = j + dy * k
+
+                print(x + 1, y + 1)
+
+            exit()
+
+print("No")
