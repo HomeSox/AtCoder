6e69c087e8d3299cb3d061dcf9af0b96b2527a5b
satory074 <satory074@gmail.com>
2023-07-09 23:56:05 +0900

    Check if the given matrix is correct

--- satory074/abc261/b/main.py
+++ satory074/abc261/b/main.py
@@ -1,2 +1,29 @@
 import collections as cl
 import math
+
+N = int(input())
+A = [list(input()) for _ in range(N)]
+
+# print(A)
+
+for i in range(N):
+    for j in range(N):
+        s = A[i][j]
+
+        if s == "-":
+            continue
+
+        if s == "W":
+            t = "L"
+        elif s == "L":
+            t = "W"
+        else:
+            t = "D"
+
+        if A[j][i] == t:
+            continue
+        else:
+            print("incorrect")
+            exit()
+
+print("correct")
