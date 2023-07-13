28de57dd8cbaa0195eea1a784b41f3084f9df702
satory074 <satory074@gmail.com>
2023-07-13 23:58:45 +0900

    Add solution for calculating minimum number of coins needed

--- satory074/abc095/b/main.py
+++ satory074/abc095/b/main.py
@@ -1,3 +1,15 @@
-import math
 import collections as cl
+import math
 from collections import deque
+
+N, X = map(int, input().split())
+m = [int(input()) for _ in range(N)]
+
+ans = 0
+
+ans += N
+X -= sum(m)
+
+ans += X // min(m)
+
+print(ans)
