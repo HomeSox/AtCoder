e1f958d6140247f2bd4f60de6b95b033fca5883a
satory074 <satory074@gmail.com>
2023-07-27 23:12:13 +0900

    Add logic to count the longest consecutive sequence of "x" in a grid

--- satory074/abc311/b/main.py
+++ satory074/abc311/b/main.py
@@ -1,2 +1,20 @@
 import collections as cl
 import math
+
+N, D = map(int, input().split())
+S = [input() for _ in range(N)]
+
+ans = 0
+cur = 0
+for d in range(D):
+    for n in range(N):
+        if S[n][d] == "x":
+            ans = max(ans, cur)
+
+            cur = 0
+
+            break
+    else:
+        cur += 1
+
+print(max(ans, cur))
