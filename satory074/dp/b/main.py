69a8bf8f295cae1e04cd6b8502b1c110e6cef370
satory074 <satory074@gmail.com>
2023-08-04 23:37:14 +0900

    Implement dynamic programming algorithm for calculating the minimum cost

--- satory074/dp/b/main.py
+++ satory074/dp/b/main.py
@@ -1,3 +1,23 @@
 import math
-import collections as cl
-from collections import deque
+
+N, K = map(int, input().split())
+h = list(map(int, input().split()))
+
+if N <= K:
+    print(abs(h[N - 1] - h[0]))
+    exit()
+
+dp = [0 for _ in range(N + 1)]
+
+for i in range(1, K + 1):
+    dp[i] = abs(h[i] - h[0])
+
+for i in range(K + 1, N):
+    min_ = math.inf
+    for k in range(1, K + 1):
+        min_ = min(min_, dp[i - k] + abs(h[i] - h[i - k]))
+
+    dp[i] = min_
+
+# print(dp)
+print(dp[N - 1])
