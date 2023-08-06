141f363ede891455e51912599053f7d1618bf1ff
satory074 <satory074@gmail.com>
2023-08-07 01:50:33 +0900

    Refactor main.py for satory074/dp/c and satory074/dp/dRefactor the code in main.py for the satory074/dp/c and satory074/dp/d directories

--- satory074/dp/c/main.py
+++ satory074/dp/c/main.py
@@ -1,3 +1,17 @@
-import math
 import collections as cl
+import math
 from collections import deque
+
+N = int(input())
+abc = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0 for _ in range(3)] for _ in range(N)]
+dp[0] = abc[0]
+
+for i in range(1, N):
+    dp[i][0] = max(dp[i - 1][1], dp[i - 1][2]) + abc[i][0]
+    dp[i][1] = max(dp[i - 1][0], dp[i - 1][2]) + abc[i][1]
+    dp[i][2] = max(dp[i - 1][0], dp[i - 1][1]) + abc[i][2]
+
+print(max(dp[N - 1]))
+
--- satory074/dp/d/main.py
+++ satory074/dp/d/main.py
@@ -1,3 +1,18 @@
-import math
 import collections as cl
+import math
 from collections import deque
+
+N, W = map(int, input().split())
+
+wv = [list(map(int, input().split())) for _ in range(N)]
+
+dp = [[0] * (W + 1) for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    for j in range(1, W + 1):
+        if j - wv[i - 1][0] >= 0:
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - wv[i - 1][0]] + wv[i - 1][1])
+        else:
+            dp[i][j] = dp[i - 1][j]
+
+print(dp[N][W])
