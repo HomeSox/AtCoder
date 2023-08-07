af63585cb06d4526824da0d0170128e9eb50b390
satory074 <satory074@gmail.com>
2023-08-08 01:30:23 +0900

    Add longest common subsequence algorithm

--- satory074/dp/f/main.py
+++ satory074/dp/f/main.py
@@ -1,3 +1,25 @@
-import math
-import collections as cl
-from collections import deque
+s = input()
+t = input()
+
+dp = [[0] * (len(t) + 1) for _ in range(len(s) + 1)]
+
+for i in range(1, len(s) + 1):
+    for j in range(1, len(t) + 1):
+        if s[i - 1] == t[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+i, j = len(s), len(t)
+ans = ""
+while i > 0 and j > 0:
+    if dp[i][j] == dp[i - 1][j]:
+        i -= 1
+    elif dp[i][j] == dp[i][j - 1]:
+        j -= 1
+    else:
+        ans = s[i - 1] + ans
+        i -= 1
+        j -= 1
+
+print(ans)
