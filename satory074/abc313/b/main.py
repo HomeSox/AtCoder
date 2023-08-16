2d6896a7c44b3e20d3a06b8ce7535baf512e4ebc
satory074 <satory074@gmail.com>
2023-08-16 23:58:22 +0900

    Add code to find the first node with zero outdegree

--- satory074/abc313/b/main.py
+++ satory074/abc313/b/main.py
@@ -1,2 +1,21 @@
-import collections as cl
-import math
+N, M = map(int, input().split())
+deg = [0 for _ in range(N)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    a -= 1
+    b -= 1
+    deg[b] += 1
+
+# print(deg)
+
+ans = -1
+for i in range(N):
+    if deg[i] == 0:
+        if ans != -1:
+            print(-1)
+            exit()
+        else:
+            ans = i + 1
+
+print(ans)
