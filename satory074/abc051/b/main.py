93c9265b7f5fae4f3692ff74a54c2a6b8d5f0819
satory074 <satory074@gmail.com>
2023-08-18 05:50:28 +0900

    Add solution to count valid combinations of x, y, and z

--- satory074/abc051/b/main.py
+++ satory074/abc051/b/main.py
@@ -1,2 +1,10 @@
-import collections as cl
-import math
+K, S = map(int, input().split())
+
+ans = 0
+for x in range(K + 1):
+    for y in range(K + 1):
+        z = S - x - y
+        if 0 <= z <= K:
+            ans += 1
+
+print(ans)
