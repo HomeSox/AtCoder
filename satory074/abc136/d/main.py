ab88efc825d7cb587be784ba7a9a262e6c50652f
satory074 <satory074@gmail.com>
2023-08-19 10:13:02 +0900

    Restructure code for efficiency and readability

--- satory074/abc136/d/main.py
+++ satory074/abc136/d/main.py
@@ -1,13 +1,20 @@
-#!/usr/bin/env python3
-import collections
-import itertools as it
-import math
-#import numpy as np
- 
-#  = input()
-#  = int(input())
-#  = map(int, input().split())
-#  = list(map(int, input().split()))
-#  = [int(input()) for i in range(N)]
-#
-# c = collections.Counter()
+S = input()
+N = len(S)
+
+dp = [[0 for _ in range(N)] for _ in range(33)]
+ans = [0 for _ in range(N)]
+
+for i in range(N):
+    if S[i] == 'R':
+        dp[0][i] = i + 1
+    else:
+        dp[0][i] = i - 1
+
+for p in range(32):
+    for i in range(N):
+        dp[p + 1][i] = dp[p][dp[p][i]]
+
+for i in range(N):
+    ans[dp[32][i]] += 1
+
+print(*ans)
