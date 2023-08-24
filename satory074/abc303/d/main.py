720089e493d99ab2a21f8d550f019b8b4911826a
satory074 <satory074@gmail.com>
2023-08-24 23:45:40 +0900

    Add dynamic programming solution for ABC303D

--- satory074/abc303/d/main.py
+++ satory074/abc303/d/main.py
@@ -1,2 +1,15 @@
-import collections as cl
-import math
+X, Y, Z = list(map(int, input().split()))
+S = input()
+
+dp = [[0, 0] for _ in range(len(S) + 1)]
+dp[0][1] = Z
+
+for i, s in enumerate(S):
+    if s == "a":
+        dp[i + 1][0] = min(dp[i][0] + X, dp[i][1] + Z + X)
+        dp[i + 1][1] = min(dp[i][1] + Y, dp[i][0] + Z + Y)
+    else:
+        dp[i + 1][0] = min(dp[i][0] + Y, dp[i][1] + Z + Y)
+        dp[i + 1][1] = min(dp[i][1] + X, dp[i][0] + Z + X)
+
+print(min(dp[len(S)]))
