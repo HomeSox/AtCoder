6215835071bb0ffc04e797ebe247ab3116a5b7e1
satory074 <satory074@gmail.com>
2023-09-01 23:50:36 +0900

    Add counter for duplicate strings in main.py

--- satory074/abc261/c/main.py
+++ satory074/abc261/c/main.py
@@ -1,2 +1,14 @@
 import collections as cl
 import math
+
+N = int(input())
+l = {}
+for i in range(N):
+    S = input()
+
+    if S in l:
+        print(f"{S}({l[S]})")
+        l[S] += 1
+    else:
+        print(S)
+        l[S] = 1
