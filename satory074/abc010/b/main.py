b7cc5737364d65cd35c170014e799a8c2eb847f1
satory074 <satory074@gmail.com>
2023-09-03 21:36:47 +0900

    Refactor code to calculate the sum of specific numbers efficiently

--- satory074/abc010/b/main.py
+++ satory074/abc010/b/main.py
@@ -1,2 +1,24 @@
 import collections as cl
 import math
+
+n = int(input())
+a = list(map(int, input().split()))
+
+ans = 0
+for a_ in a:
+    if a_ in [1, 3, 7, 9]:
+        continue
+
+    if a_ in [2, 4, 8]:
+        ans += 1
+        continue
+
+    if a_ in [5]:
+        ans += 2
+        continue
+
+    if a_ in [6]:
+        ans += 3
+        continue
+
+print(ans)
