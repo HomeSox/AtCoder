28477b9e865ce81fe931094d35614d3a0d8c67e1
satory074 <satory074@gmail.com>
2023-09-06 23:39:04 +0900

    "Add graph initialization and counting logic"

--- satory074/abc303/b/main.py
+++ satory074/abc303/b/main.py
@@ -1,2 +1,26 @@
 import collections as cl
 import math
+
+N, M = map(int, input().split())
+
+l = {}
+for a in range(1, N + 1):
+    for b in range(1, N + 1):
+        if a == b:
+            continue
+
+        l[a * 100 + b] = False
+
+for _ in range(M):
+    a = list(map(int, input().split()))
+
+    for a1, a2 in zip(a[:-1], a[1:]):
+        l[a1 * 100 + a2] = True
+        l[a2 * 100 + a1] = True
+
+ans = 0
+for l_ in l.values():
+    if not l_:
+        ans += 1
+
+print(ans // 2)
