9fb5d3fc3034d7734c7b28540cbddee15a99a31b
Cer0un0 <wataru.mrt@gmail.com>
2023-09-07 23:56:29 +0900

    add:ABC317 A

---
+++ Cer0un0/ABC317/a/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+N, H, X = map(int, input().split())
+P = list(map(int, input().split()))
+
+for i, p in enumerate(P):
+    if H + p >= X:
+        print(i + 1)
+        exit()
