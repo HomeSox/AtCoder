9afe11e9502caa1bba6d940f8f968697215343eb
satory074 <satory074@gmail.com>
2023-09-13 00:28:44 +0900

    Add logic to count the number of rectangles formed

--- satory074/abc218/d/main.py
+++ satory074/abc218/d/main.py
@@ -1,1 +1,22 @@
-# = map(int, input())
+N = int(input())
+
+xy = []
+for _ in range(N):
+    x, y = map(int, input().split())
+    xy.append((x, y))
+
+xy_set = set(xy)
+
+ans = 0
+for x1, y1 in xy:
+    for x2, y2 in xy:
+        if x1 >= x2:
+            continue
+
+        if y1 >= y2:
+            continue
+
+        if (x1, y2) in xy_set and (x2, y1) in xy_set:
+            ans += 1
+
+print(ans)
