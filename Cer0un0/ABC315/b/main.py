1394c7881d30d1b6921e1893ebd6ad5eec548681
Cer0un0 <wataru.mrt@gmail.com>
2023-09-13 20:57:58 +0900

    add: ABC315 B

--- Cer0un0/ABC315/b/main.py
+++ Cer0un0/ABC315/b/main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
-N = int(input())
-S = input()
-a, b = map(int, input().split())
-A = list(map(int, input().split()))
-B = [int(input()) for i in range(N)]
-d = [0 for i in range(N)]
+M = int(input())
+D = list(map(int, input().split()))
+sum_d = sum(D)
+mid = sum_d // 2 + 1
+v = 0
+mid_i = 0
+ans = []
+for i in range(M):
+    v += D[i]
+    if v >= mid:
+        mid_i = i + 1
+        break
+
+print(*[mid_i, mid-(v-D[mid_i-1])])
