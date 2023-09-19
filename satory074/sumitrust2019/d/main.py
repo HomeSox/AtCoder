de170359163e64a152a2816f2931a005354584d9
satory074 <satory074@gmail.com>
2023-09-19 23:16:42 +0900

    sumitrust2019/d

--- satory074/sumitrust2019/d/main.py
+++ satory074/sumitrust2019/d/main.py
@@ -1,13 +1,23 @@
-#!/usr/bin/env python3
-import collections
-import itertools as it
-import math
-#import numpy as np
- 
-#  = input()
-#  = int(input())
-#  = map(int, input().split())
-#  = list(map(int, input().split()))
-#  = [int(input()) for i in range(N)]
-#
-# c = collections.Counter()
+N = int(input())
+S = input()
+
+ans = 0
+for i in range(1000):
+    key = str(i).zfill(3)
+
+    n1 = S.find(key[0])
+
+    if n1 == -1:
+        continue
+
+    n2 = S.find(key[1], n1 + 1)
+    if n2 == -1:
+        continue
+
+    n3 = S.find(key[2], n2 + 1)
+    if n3 == -1:
+        continue
+
+    ans += 1
+
+print(ans)
