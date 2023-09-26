0a4e6b6c985cc16a152fc738bca6d8a17a56415a
satory074 <satory074@gmail.com>
2023-09-27 00:58:06 +0900

    Refactor code with clearer variable names and remove unnecessary imports

--- satory074/abc311/c/main.py
+++ satory074/abc311/c/main.py
@@ -1,2 +1,18 @@
-import collections as cl
-import math
+N = int(input())
+A = list(map(int, input().split()))
+A = [0] + A
+
+start = 1
+l = [start]
+visited = {start: 0}
+while True:
+    next = A[l[-1]]
+    # print(next, l)
+    if next in visited:
+        idx = visited[next]
+        print(len(l[idx:]))
+        print(*l[idx:])
+        exit()
+    else:
+        l.append(next)
+        visited[next] = len(l) - 1
--- satory074/abc311/d/main.py
+++ satory074/abc311/d/main.py
@@ -1,2 +1,29 @@
-import collections as cl
-import math
+N, M = map(int, input().split())
+S = [input() for _ in range(N)]
+ans = [[False for _ in range(M)] for _ in range(N)]
+
+start = (1, 1)
+l = [start]
+visited = set()
+
+ans[start[0]][start[1]] = True
+
+while l:
+    y, x = l.pop()
+
+    visited.add((y, x))
+
+    for dy, dx in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        y_, x_ = y, x
+
+        while 0 <= y_ < N and 0 <= x_ < M and S[y_][x_] != "#":
+            ans[y_][x_] = True
+            y_ += dy
+            x_ += dx
+
+        y__, x__ = (y_ - dy, x_ - dx)
+        if 0 <= y__ < N and 0 <= x__ < M and (y__, x__) not in visited:
+            l.append((y__, x__))
+
+c = sum([row.count(True) for row in ans])
+print(c)
