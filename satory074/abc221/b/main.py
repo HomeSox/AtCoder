189e7a742d27f624a6b2dbc8dcc4a43f604b5399
satory074 <satory074@gmail.com>
2023-09-29 08:46:17 +0900

    feat: abs221/b

--- satory074/abc221/b/main.py
+++ satory074/abc221/b/main.py
@@ -1,2 +1,14 @@
-import collections as cl
-import math
+S = input()
+T = input()
+
+ans = 0
+for i in range(len(S) - 1):
+  if S[i] != T[i]:
+    if S[i] == T[i + 1] and S[i + 1] == T[i]:
+      print('Yes')
+    else:
+      print('No')
+      
+    exit()
+
+print('Yes')
