ad74961345eb7eacf522de2cdad37a67c7c8c64c
satory074 <satory074@gmail.com>
2023-10-03 02:00:40 +0900

    Add input validation for PCF values in main.py

--- satory074/abc310/b/main.py
+++ satory074/abc310/b/main.py
@@ -1,2 +1,20 @@
-import collections as cl
-import math
+N, M = map(int, input().split())
+PCF = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        if i == j:
+            continue
+
+        Pi, Ci, *Fi = PCF[i]
+        Pj, Cj, *Fj = PCF[j]
+
+        li = set(Fi)
+        lj = set(Fj)
+
+        if Pj <= Pi and li.issubset(lj):
+            if Pj < Pi or lj - li:
+                print('Yes')
+                exit()
+
+print('No')
