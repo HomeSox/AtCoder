794f0d3d85df0f4573d22bc3f52b0de25899c4e0
satory074 <satory074@gmail.com>
2023-10-21 12:16:37 +0900

    Refactor code to improve readability and remove unnecessary comments

--- satory074/abc323/c/main.py
+++ satory074/abc323/c/main.py
@@ -1,2 +1,42 @@
-import collections as cl
-import math
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+
+answers = []
+for _ in range(N):
+    answers.append(list(input()))
+
+points = [0] * N
+mada_task_point = [[] for _ in range(N)]
+for i, a in enumerate(answers):
+    p = i + 1
+
+    for j, s in enumerate(answers[i]):
+        if s == "o":
+            p += A[j]
+        else:
+            mada_task_point[i].append(A[j])
+
+    points[i] = p
+    mada_task_point[i].sort(reverse=True)
+
+# print(points)
+# print(mada_task_point)
+
+border_point = max(points)
+# print(border_point)
+
+for i, (mada, point) in enumerate(zip(mada_task_point, points)):
+    diff = border_point - point
+
+    c = 0
+    for m in mada:
+        if diff <= 0:
+            print(c)
+            break
+
+        diff -= m
+        c += 1
+    else:
+        print(c)
+
+    # print(mada, point)
--- satory074/abc323/d/main.py
+++ satory074/abc323/d/main.py
@@ -1,2 +1,32 @@
-import collections as cl
-import math
+import bisect as bs
+
+N = int(input())
+
+slimes = {}
+gouseiable = []
+for _ in range(N):
+    S, C = map(int, input().split())
+
+    slimes[S] = C
+
+    if C != 1:
+        gouseiable.append(S)
+
+gouseiable.sort()
+
+while gouseiable:
+    s = gouseiable.pop()
+
+    if s * 2 in slimes:
+        slimes[s * 2] += slimes[s] // 2
+    else:
+        slimes[s * 2] = slimes[s] // 2
+
+    if slimes[s * 2] >= 2:
+        bs.insort_left(gouseiable, s * 2)
+
+    slimes[s] = slimes[s] % 2
+
+# print(slimes)
+
+print(sum(slimes.values()))
