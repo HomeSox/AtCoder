ac954350acc4befb65bc346599daa7a59c135182
satory074 <satory074@gmail.com>
2023-10-30 09:35:16 +0900

    Update main.py

--- satory074/abc301/b/main.py
+++ satory074/abc301/b/main.py
@@ -1,2 +1,11 @@
-import collections as cl
-import math
+N = int(input())
+A = list(map(int, input().split()))
+
+l = []
+for a1, a2 in zip(A[:-1], A[1:]):
+  for i in range(a1, a2, (a2 - a1) // abs(a2 - a1)):
+    l.append(i)
+
+l.append(A[-1])
+
+print(*l)
