44a735783a94b300c0f67e20ef3021be86bfd3de
satory074 <satory074@gmail.com>
2023-11-01 06:43:24 +0900

    Sort and count numbers with matching digits

--- satory074/abc324/d/main.py
+++ satory074/abc324/d/main.py
@@ -1,2 +1,14 @@
-import collections as cl
-import math
+N = int(input())
+S = list(input())
+
+S.sort()
+ans = 0
+for n in range(int((10 ** N) ** 0.5) + 1):
+    s = list(str(n * n).zfill(N))
+    s.sort()
+
+    if s == S:
+        ans += 1
+        # print(n * n)
+
+print(ans)
