cc143ea9fc041cee497e6349e33dae8290245ff8
satory074 <satory074@gmail.com>
2023-11-07 11:07:57 +0900

    Update main.py

--- satory074/abc299/b/main.py
+++ satory074/abc299/b/main.py
@@ -1,2 +1,16 @@
-import collections as cl
-import math
+N, T = map(int, input().split())
+C = list(map(int, input().split()))
+R = list(map(int, input().split()))
+
+color_ = T if T in C else C[0]
+
+ans = 1
+max_ = -1
+for i in range(N):
+  if C[i] == color_:
+    if R[i] > max_:
+      max_ = R[i]
+      ans = i + 1
+
+print(color_)
+print(ans)
