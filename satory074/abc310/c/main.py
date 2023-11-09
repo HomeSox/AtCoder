28326bca32d024f5af76572e85af552b32a7a407
satory074 <satory074@gmail.com>
2023-11-09 15:15:26 +0900

    Update main.py

--- satory074/abc310/c/main.py
+++ satory074/abc310/c/main.py
@@ -1,2 +1,17 @@
-import collections as cl
-import math
+N = int(input())
+dic = {}
+
+l = []
+for _ in range(N):
+  S = input()
+  
+  if len(S) == 1:
+    S_ = S
+  else:
+    S_ = min(S, S[::-1])
+  
+  l.append(S_)
+
+# print(l)
+# print(set(l))
+print(len(set(l)))
