0327f608911dafb90f4ef3e318ae58328ef703b5
satory074 <satory074@gmail.com>
2023-11-14 10:14:22 +0900

    Update main.py

--- satory074/abc314/c/main.py
+++ satory074/abc314/c/main.py
@@ -1,2 +1,24 @@
-import collections as cl
-import math
+from collections import deque
+import sys
+
+N, M = map(int, input().split())
+S = input()
+C = list(map(int, input().split()))
+
+colors = [deque() for _ in range(M + 1)]
+for i in range(N):
+    colors[C[i]].append(S[i])
+
+for i in range(1, M + 1):
+    if not colors[i]:
+        continue
+
+    colors[i].appendleft(colors[i].pop())
+
+# print(colors)
+
+ans = ""
+for i in range(N):
+    ans += colors[C[i]].popleft()
+
+print(ans)
