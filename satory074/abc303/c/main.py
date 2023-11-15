1b5e98fb4b87d02f1137b180e88ca721bdc3f892
satory074 <satory074@gmail.com>
2023-11-15 15:46:15 +0900

    Update main.py

--- satory074/abc303/c/main.py
+++ satory074/abc303/c/main.py
@@ -1,2 +1,28 @@
-import collections as cl
-import math
+N, M, H, K = map(int, input().split())
+S = input()
+xy = {tuple(map(int, input().split())) for _ in range(M)}
+
+visited = set()
+curx, cury = 0, 0
+for s in S:
+    if s == 'R':
+        curx += 1
+    elif s == 'L':
+        curx -= 1
+    elif s == 'U':
+        cury += 1
+    elif s == 'D':
+        cury -= 1
+
+    H -= 1
+
+    if H < 0:
+        print('No')
+        exit()
+
+    cur = (curx, cury)
+    if H < K and cur in xy and cur not in visited:
+        H = K
+        visited.add(current_pos)
+
+print('Yes')
