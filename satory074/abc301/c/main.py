bb6f766cfa22a1798ce71ddd1ef2e711206da63a
satory074 <satory074@gmail.com>
2023-11-19 12:10:15 +0900

    abc301/c

--- satory074/abc301/c/main.py
+++ satory074/abc301/c/main.py
@@ -1,2 +1,29 @@
 import collections as cl
 import math
+
+S = input()
+T = input()
+
+scnt = cl.Counter(S)
+tcnt = cl.Counter(T)
+
+for c in 'atcoder':
+    ma = max(scnt[c], tcnt[c])
+
+    if scnt['@'] < ma - scnt[c]:
+        print('No')
+        exit()
+
+    if tcnt['@'] < ma - tcnt[c]:
+        print('No')
+        exit()
+
+    scnt['@'] -= ma - scnt[c]
+    scnt[c] = ma
+    tcnt['@'] -= ma - tcnt[c]
+    tcnt[c] = ma
+
+if scnt == tcnt:
+    print('Yes')
+else:
+    print('No')
