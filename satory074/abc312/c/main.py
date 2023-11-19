f5bed04f039397f615ce97b6215d4f84e83cd007
satory074 <satory074@gmail.com>
2023-11-19 13:54:04 +0900

    abc312/c

--- satory074/abc312/c/main.py
+++ satory074/abc312/c/main.py
@@ -1,2 +1,20 @@
-import collections as cl
-import math
+N, M = list(map(int, input().split()))
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort()
+B.sort()
+
+l, r =  0, 10 ** 9 + 1
+while r - l > 1:
+    m = (l + r) // 2
+
+    c_A = len([i for i in A if i <= m])
+    c_B = len([i for i in B if i >= m])
+
+    if c_A >= c_B:
+        r = m
+    else:
+        l = m
+
+print(r)
