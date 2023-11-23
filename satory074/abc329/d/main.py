9138b2d3111558ab26ad7283b156c2b6a57527ca
satory074 <satory074@gmail.com>
2023-11-24 04:26:13 +0900

    feat: abc329/d

--- satory074/abc329/d/main.py
+++ satory074/abc329/d/main.py
@@ -5,8 +5,20 @@ from collections import deque
 
 sys.setrecursionlimit(10 ** 6)
 
-= input()
-= int(input())
-= map(int, input().split())
-= list(map(int, input().split()))
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+
+cnt = [0] * (N + 1)  # 各候補者の得票数
+ans = 0  # 現時点での当選者
+results = []
+
+for vote in A:
+    cnt[vote] += 1
+    if cnt[ans] < cnt[vote]:
+        ans = vote
+    elif cnt[ans] == cnt[vote]:
+        ans = min(ans, vote)
+
+    print(ans)
+
 
