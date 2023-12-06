9b949f2e4acf2450b29049c9af4ff902baa9ef5a
satory074 <satory074@gmail.com>
2023-12-06 11:26:04 +0900

    Update main.py

--- satory074/abc232/b/main.py
+++ satory074/abc232/b/main.py
@@ -1,2 +1,13 @@
-import collections as cl
-from collections import deque
+S = input()
+T = input()
+
+K = (ord(S[0]) - ord(T[0]) + 26) % 26
+
+for s, t in zip(S, T):
+  if (ord(s) - ord(t) + 26) % 26 == K:
+    continue
+  
+  print('No')
+  exit()
+
+print('Yes')
