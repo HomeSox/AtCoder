a221d23720a9e8c7e6457378c47dad7d71590e23
satory074 <satory074@gmail.com>
2023-12-07 16:24:57 +0900

    Update main.py

--- satory074/abc179/b/main.py
+++ satory074/abc179/b/main.py
@@ -1,2 +1,17 @@
-import collections as cl
-import math
+N = int(input())
+cnt = 0
+
+for _ in range(N):
+  D1, D2 = map(int, input().split())
+  
+  if D1 == D2:
+    cnt += 1
+    
+    if cnt == 3:
+      print('Yes')
+      exit()
+  
+  else:
+    cnt = 0
+
+print('No')
