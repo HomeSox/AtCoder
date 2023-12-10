6625bc885beccec47cab45d49cf0c1c4a3770590
satory074 <satory074@gmail.com>
2023-12-11 01:03:38 +0900

    feat: abc331/b

--- satory074/abc331/b/main.py
+++ satory074/abc331/b/main.py
@@ -5,8 +5,13 @@ from collections import deque
 
 sys.setrecursionlimit(10 ** 6)
 
-= input()
-= int(input())
-= map(int, input().split())
-= list(map(int, input().split()))
+N, S, M, L = map(int, input().split())
 
+ans = 10 ** 18
+for i in range(101):
+    for j in range(101):
+        for k in range(101):
+            if i * 6 + j * 8 + k * 12 >= N:
+                ans = min(ans, S * i + M * j + L * k)
+
+print(ans)
