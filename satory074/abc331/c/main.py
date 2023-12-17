b5b86c3d2f1a64de684c7e2eafd2dde1faa04cdb
satory074 <satory074@gmail.com>
2023-12-17 17:10:58 +0900

    feat:abc331/c

--- satory074/abc331/c/main.py
+++ satory074/abc331/c/main.py
@@ -5,8 +5,24 @@ from collections import deque
 
 sys.setrecursionlimit(10 ** 6)
 
-= input()
-= int(input())
-= map(int, input().split())
-= list(map(int, input().split()))
+N = int(input())
+A = list(map(int, input().split()))
+
+clc = cl.Counter(A)
+# print(clc)
+
+l_nnum = [0 for _ in range(10 ** 6 + 1)]
+for k, v in clc.items():
+    l_nnum[k] = k * v
+
+# 累積和
+Q = [0 for _ in range(10 ** 6 + 1)]
+for i in range(1, 10 ** 6 + 1):
+    Q[i] = Q[i - 1] + l_nnum[i]
+
+ans = []
+for a in A:
+    ans.append(Q[-1] - Q[a])
+
+print(*ans)
 
