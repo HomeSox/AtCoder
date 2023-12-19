a92b2df9dab8a185e771226de995756081329ac3
satory074 <satory074@gmail.com>
2023-12-19 11:22:06 +0900

    Update main.py

--- satory074/abc299/c/main.py
+++ satory074/abc299/c/main.py
@@ -1,2 +1,20 @@
-import collections as cl
-import math
+N = int(input())
+S = input()
+
+ans = 0
+
+for flip in range(2):
+    level = 0
+    for i in range(N):
+        if S[i] == '-':
+            ans = max(ans, level)
+            level = 0
+        else:
+            level += 1
+    
+    S = S[::-1]
+
+if ans:
+    print(ans)
+else:
+    print(-1)
