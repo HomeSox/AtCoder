37184aea3243c3449080276a2d5f74c92bd47bbe
satory074 <satory074@gmail.com>
2023-12-23 03:56:10 +0900

    feat: abc317/d

--- satory074/abc317/d/main.py
+++ satory074/abc317/d/main.py
@@ -1,2 +1,15 @@
-import collections as cl
-import math
+N = int(input())
+XYZ = [list(map(int, input().split())) for _ in range(N)]
+
+sumZ = sum(Z for X, Y, Z in XYZ)
+
+dp = [float('inf') for _ in range(sumZ + 1)]
+dp[0] = 0
+
+for x, y, z in XYZ:
+    w = (y - x + 1) // 2 if x < y else 0
+
+    for i in range(sumZ, z - 1, -1):
+        dp[i] = min(dp[i], dp[i - z] + w)
+
+print(min(dp[(sumZ + 1) // 2:]))
