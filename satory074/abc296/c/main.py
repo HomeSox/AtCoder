53a7f9c0f90e4c27b9f249e63ac856a605ced7ff
satory074 <satory074@gmail.com>
2023-12-24 02:37:12 +0900

    abc296/c

--- satory074/abc296/c/main.py
+++ satory074/abc296/c/main.py
@@ -1,2 +1,17 @@
 import collections as cl
 import math
+
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+A.sort()
+i = 0
+for j in range(len(A)):
+    while i < len(A) and A[i] - A[j] < X:
+        i += 1
+
+    if i < len(A) and A[i] - A[j] == X:
+        print("Yes")
+        exit()
+
+print("No")
