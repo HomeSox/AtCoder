12f9b3166ea70bc4948b65f512a435eab85902f0
satory074 <satory074@gmail.com>
2024-01-03 17:59:47 +0900

    feat:abc333/c

--- satory074/abc333/c/main.py
+++ satory074/abc333/c/main.py
@@ -5,8 +5,12 @@ from collections import deque
 
 sys.setrecursionlimit(10 ** 6)
 
-= input()
-= int(input())
-= map(int, input().split())
-= list(map(int, input().split()))
-
+N = int(input())
+L = 12
+r = [int("1" * (i + 1)) for i in range(L)]
+for i in range(L):
+    for j in range(i + 1):
+        for k in range(j + 1):
+            N -= 1
+            if N == 0:
+                print(r[i] + r[j] + r[k])
