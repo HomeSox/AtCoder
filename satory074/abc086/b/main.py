54630a1d016e50ca17e3bc2b1a7f1d177dc10ac3
satory074 <satory074@gmail.com>
2024-01-04 00:52:06 +0900

    feat: abc086/b

--- satory074/abc086/b/main.py
+++ satory074/abc086/b/main.py
@@ -1,3 +1,13 @@
-import math
 import collections as cl
+import math
 from collections import deque
+
+a, b = input().split()
+n = int(a + b)
+
+for i in range(1000):
+    if i ** 2 == n:
+        print("Yes")
+        exit()
+
+print("No")
