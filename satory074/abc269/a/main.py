7ef0e446cfe3fb5493e7c004fd9663f32aa1b396
satory074 <satory074@gmail.com>
2024-01-29 02:14:15 +0900

    abc269/a

---
+++ satory074/abc269/a/main.py
@@ -0,0 +1,12 @@
+import collections as cl
+import math
+import sys
+from collections import deque
+
+sys.setrecursionlimit(10 ** 6)
+
+a, b, c, d = map(int, input().split())
+
+print((a + b) * (c - d))
+print('Takahashi')
+
