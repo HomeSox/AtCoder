3082731ffa7ecc5abefc1b5dec404f8bfe253ffd
satory074 <satory074@gmail.com>
2024-02-01 14:21:59 +0900

    Update main.py

--- satory074/abc120/b/main.py
+++ satory074/abc120/b/main.py
@@ -1,3 +1,8 @@
-import math
-import collections as cl
-from collections import deque
+A, B, K = map(int, input().split())
+
+l = []
+for i in range(1, min(A, B) + 1):
+    if A % i == 0 and B % i == 0:
+        l.append(i)
+
+print(l[::-1][K-1])
