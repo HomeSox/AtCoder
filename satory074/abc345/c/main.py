7fb9f16481adcafcca3994579366a1ef1d7bbbdd
satory074 <satory074@gmail.com>
2024-03-20 14:19:35 +0900

    abc345/c

--- satory074/abc345/c/main.py
+++ satory074/abc345/c/main.py
@@ -1,12 +1,21 @@
 import collections as cl
 import math
 import sys
-from collections import deque
+from collections import Counter, deque
 
 sys.setrecursionlimit(10 ** 6)
 
-= input()
-= int(input())
-= map(int, input().split())
-= list(map(int, input().split()))
+S = input()
+cnt = Counter(S)
 
+
+same = False  # 同じ文字が2回以上含まれているかどうか
+
+n = len(S)
+ans = n * n - sum(c * c for c in cnt.values())
+ans //= 2
+
+if len(S) != len(cnt):
+    ans += 1
+
+print(ans)
