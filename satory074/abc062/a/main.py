7196fa12fe938331591b4181b7ae2627d8dadfc0
satory074 <satory074@gmail.com>
2024-03-23 13:39:34 +0900

    Update main.py

--- satory074/abc062/a/main.py
+++ satory074/abc062/a/main.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
-import collections
-import itertools as it
-import math
-#import numpy as np
- 
-#  = input()
-#  = int(input())
-#  = map(int, input().split())
-#  = list(map(int, input().split()))
-#  = [int(input()) for i in range(N)]
-#
-# c = collections.Counter()
+l = [[1, 3, 5, 7, 8, 10, 12], [4, 6, 9, 11], [2]]
+x, y = map(int, input().split())
+
+for l_ in l:
+  if x in l_:
+    if y in l_:
+      print('Yes')
+    else:
+      print('No')
+    
+    break
