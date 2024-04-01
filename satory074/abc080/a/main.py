3de2192b69cdab56b316f368defd954e58fbb24a
satory074 <satory074@gmail.com>
2024-04-01 11:57:11 +0900

    Update main.py

--- satory074/abc080/a/main.py
+++ satory074/abc080/a/main.py
@@ -1,13 +1,3 @@
-#!/usr/bin/env pypy3
-import collections
-import itertools as it
-import math
-#import numpy as np
- 
-#  = input()
-#  = int(input())
-#  = map(int, input().split())
-#  = list(map(int, input().split()))
-#  = [int(input()) for i in range(N)]
-#
-# c = collections.Counter()
+N, A, B = map(int, input().split())
+
+print(min(A * N, B))
