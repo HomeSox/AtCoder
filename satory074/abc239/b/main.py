a8a9e7142b33a82e1a09589220ceda21946209e6
satory074 <satory074@gmail.com>
2024-04-11 09:18:08 +0900

    Update main.py

--- satory074/abc239/b/main.py
+++ satory074/abc239/b/main.py
@@ -1,12 +1,2 @@
-import collections as cl
-import math
-import sys
-from collections import deque
-
-sys.setrecursionlimit(10 ** 6)
-
-= input()
-= int(input())
-= map(int, input().split())
-= list(map(int, input().split()))
-
+X = int(input())
+print(X // 10)
