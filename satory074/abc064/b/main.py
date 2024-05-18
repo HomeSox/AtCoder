ac33a9437d22f4d5a7985b7fc677f68c31ac9d97
satory074 <satory074@gmail.com>
2024-05-18 13:31:49 +0900

    Update main.py

--- satory074/abc064/b/main.py
+++ satory074/abc064/b/main.py
@@ -1,13 +1,4 @@
-#!/usr/bin/env python3
-import collections
-import itertools as it
-import math
-#import numpy as np
- 
-#  = input()
-#  = int(input())
-#  = map(int, input().split())
-#  = list(map(int, input().split()))
-#  = [int(input()) for i in range(N)]
-#
-# c = collections.Counter()
+N = int(input())
+a = list(map(int, input().split()))
+
+print(max(a) - min(a))
