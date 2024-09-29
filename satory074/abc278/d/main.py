d62a9bf15a3f881fd1aaff3b59594cc6d0861494
satory074 <satory074@gmail.com>
2024-09-29 13:36:42 +0900

    abc278/d

--- satory074/abc278/d/main.py
+++ satory074/abc278/d/main.py
@@ -1,12 +1,36 @@
-import collections as cl
-import math
-import sys
-from collections import deque
+N = int(input())
+A = list(map(int, input().split()))
 
-sys.setrecursionlimit(10 ** 6)
+Q = int(input())
+all_ = None
+delta = [0] * N
+idxs = set(range(N))
 
-= input()
-= int(input())
-= map(int, input().split())
-= list(map(int, input().split()))
+for _ in range(Q):
+    query = input().split()
 
+    if query[0] == "1":
+        x = int(query[1])
+        all_ = x
+
+        for idx in idxs:
+            delta[idx] = 0
+
+        idxs.clear()
+
+    elif query[0] == "2":
+        i = int(query[1]) - 1
+        x = int(query[2])
+
+        if all_ is None:
+            A[i] += x
+        else:
+            delta[i] += x
+            idxs.add(i)
+
+    elif query[0] == "3":
+        i = int(query[1]) - 1
+        if all_ is None:
+            print(A[i])
+        else:
+            print(all_ + delta[i])
