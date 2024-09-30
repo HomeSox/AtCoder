be28a2c0921908d385b8e3b38894eba3e15736aa
satory074 <satory074@gmail.com>
2024-09-30 23:39:20 +0900

    abc171

--- satory074/abc171/c/main.py
+++ satory074/abc171/c/main.py
@@ -1,13 +1,24 @@
 N = int(input())
 
-# 27 = aa
-# 123456789 = jjddja
+k = 1
+total = 0
+while True:
+    total += 26**k
 
-# Nを26進数でprint
-# 26進数の桁数はlog26(N)で求められる
-# 26進数の各桁はNを26で割った余りで求められる
-# 26進数の各桁は文字に変換できる
-# 26進数の各桁を逆順にprint
-# 26進数の各桁を逆順にprintしたものを文字列に変換する
-# 26進数の各桁を逆順にprintしたものを文字列に変換したものをprint
-print(''.join([chr(ord('a') + (N % 26)) for N in range(N)]))
+    if N <= total:
+        break
+
+    k += 1
+
+
+total -= 26**k
+pos = N - total - 1
+
+name = []
+for _ in range(k):
+    name.append(chr(ord("a") + pos % 26))
+    pos //= 26
+
+name = "".join(reversed(name))
+
+print(name)
