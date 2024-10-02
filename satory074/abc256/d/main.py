1de9113a31bb241cc2db2aaa08628e4af09ca5b9
satory074 <satory074@gmail.com>
2024-10-03 07:32:41 +0900

    abc256/d

--- satory074/abc256/d/main.py
+++ satory074/abc256/d/main.py
@@ -1,12 +1,24 @@
-import collections as cl
-import math
-import sys
-from collections import deque
+N = int(input())
+intervals = []
+idx = 1
+for _ in range(N):
+    L, R = map(int, input().split())
+    intervals.append((L, R))
+    idx += 2
 
-sys.setrecursionlimit(10 ** 6)
+intervals.sort()
 
-= input()
-= int(input())
-= map(int, input().split())
-= list(map(int, input().split()))
+merged = []
+for interval in intervals:
+    L, R = interval
+    if not merged:
+        merged.append([L, R])
+    else:
+        last_L, last_R = merged[-1]
+        if L <= last_R:
+            merged[-1][1] = max(last_R, R)
+        else:
+            merged.append([L, R])
 
+for interval in merged:
+    print(interval[0], interval[1])
